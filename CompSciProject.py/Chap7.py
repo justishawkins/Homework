745cf5b9f157980fb4b7c75109eb2bd36ab471ef
justishawkins <32522747+justishawkins@users.noreply.github.com>
2018-01-12 10:09:29 -0500

    CompSciProject

---
+++ CompSciProject.py/Chap7.py
@@ -0,0 +1,90 @@
+classList=("Kate", "Ethan", "Tom", "Sarah", "Ben", "Alexandria")
+group1=(" "," ")
+group2=(" "," ")
+group3=(" "," ")
+choice=input("What people would you like to group together?")
+if choice=="Kate":
+    group1=("Kate","Ben")
+    print("Kate will be grouped with Ben")
+elif choice=="Ethan":
+    group1=("Ethan","Alexandria")
+    print("Ethan will be grouped with Alexandria")
+elif choice=="Ben":
+    group1=("Ben","Kate")
+    print("Ben will be grouped with Kate")
+elif choice=="Tom":
+    group1=("Tom","Sarah")
+    print("Tom will be grouped with Sarah")
+elif choice=="Sarah":
+    group1=("Sarah","Tom")
+    print("Sarah will be grouped with Tom")
+elif choice=="Alexandria":
+    group1=("Alexandria","Ethan")
+    print("Alexandria will be grouped with Ethan")
+    classList=("Kate, Ethan, Tom, Sarah, Ben, Alexandria")
+choice2=input("Anyone else?")
+if choice2=="Kate":
+    group2=("Kate","Ben")
+    print("Kate will be grouped with Ben")
+elif choice=="Ethan":
+    print("Ethan will be grouped with Alexandria")
+elif choice=="Ben":
+    print("Ben will be grouped with Kate")
+elif choice=="Tom":
+    print("Tom will be grouped with Sarah")
+elif choice=="Sarah":
+    print("Sarah will be grouped with Tom")
+elif choice=="Alexandria":
+    print("Alexandria will be grouped with Ethan")
+choice3=input("Anyone else?")
+if choice=="Kate":
+    print("Kate will be grouped with Ben")
+elif choice=="Ethan":
+    print("Ethan will be grouped with Alexandria")
+elif choice=="Ben":
+    print("Ben will be grouped with Kate")
+elif choice=="Tom":
+    print("Tom will be grouped with Sarah")
+elif choice=="Sarah":
+    print("Sarah will be grouped with Tom")
+elif choice=="Alexandria":
+    print("Alexandria will be grouped with Ethan")
+choice4=input("Anyone else?")
+if choice=="Kate":
+    print("Kate will be grouped with Ben")
+elif choice=="Ethan":
+    print("Ethan will be grouped with Alexandria")
+elif choice=="Ben":
+    print("Ben will be grouped with Kate")
+elif choice=="Tom":
+    print("Tom will be grouped with Sarah")
+elif choice=="Sarah":
+    print("Sarah will be grouped with Tom")
+elif choice=="Alexandria":
+    print("Alexandria will be grouped with Ethan")
+choice5=input("Anyone else?")
+if choice=="Kate":
+    print("Kate will be grouped with Ben")
+elif choice=="Ethan":
+    print("Ethan will be grouped with Alexandria")
+elif choice=="Ben":
+    print("Ben will be grouped with Kate")
+elif choice=="Tom":
+    print("Tom will be grouped with Sarah")
+elif choice=="Sarah":
+    print("Sarah will be grouped with Tom")
+elif choice=="Alexandria":
+    print("Alexandria will be grouped with Ethan")
+choice6=input("Anyone else?")
+if choice=="Kate":
+    print("Kate will be grouped with Ben")
+elif choice=="Ethan":
+    print("Ethan will be grouped with Alexandria")
+elif choice=="Ben":
+    print("Ben will be grouped with Kate")
+elif choice=="Tom":
+    print("Tom will be grouped with Sarah")
+elif choice=="Sarah":
+    print("Sarah will be grouped with Tom")
+elif choice=="Alexandria":
+    print("Alexandria will be grouped with Ethan")
